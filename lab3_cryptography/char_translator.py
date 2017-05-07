f8df37d1a8f094aefdbbf74d247e9f8fdc1aab7f
Gerald Shin <gdshin@gmail.com>
2017-05-06 22:54:03 -0700

    lab3 complete

--- lab3_cryptography/char_translator.py
+++ lab3_cryptography/char_translator.py
@@ -1,5 +1,5 @@
 """
-This file contains code to generate the random substitution code and to 
+This file contains code to generate the random substitution code and to
 translate and untranslate individual characters based on that code.
 """
 
@@ -12,21 +12,37 @@ def random_permute_chars():
     s = string.ascii_lowercase
     return ''.join(random.sample(s,len(s)))
 
-class CharTranslator(string):
-    def __init__(self, string):
+class CharTranslator:
+    def __init__(self, input_str):
         """ takes random a-z string and checks for valid permutation """
-        if Counter(string) == Counter(string.ascii_lowercase):
-            self.string = string
+        self.str_ordered = string.ascii_lowercase
+        if Counter(self.str_ordered) == Counter(input_str):
+            self.str_random = input_str
         else:
-            raise Exception("Not a valid permutation of a-z string")
+            raise InvalidTranslationString("Not a valid permutation of a-z string")
 
-    def translate_char(self):
-        """ empty """
+    def translate_char(self, char):
+        """ translates a single character based on permutation """
+        return self.convert_char(char, self.str_ordered, self.str_random)
 
-    def unstranslate_char(self):
-        """ empty """
+    def untranslate_char(self, char):
+        """ untranslates a single character based on permutation """
+        return self.convert_char(char, self.str_random, self.str_ordered)
 
-class InvalidTranslationString:
-    def __init(self):
-        """ empty """
+    def convert_char(self, char, a, b):
+        if len(char) is not 1:
+            raise Exception("Expect string len 1 not {}".format(str(len(char))))
+        if char.isalpha():
+            if char.isupper():
+                """ convert uppercase """
+                num_lower = ord(char) + 32
+                char_lower = a.index(chr(num_lower))
+                return b[char_lower].upper()
+            else:
+                return b[a.index(char)]
+        else:
+            return char
+
+class InvalidTranslationString(Exception):
+    pass
 
--- lab3_cryptography/crypto.py
+++ lab3_cryptography/crypto.py
@@ -1,12 +1,35 @@
-#!/bin/bash
 
 """
 This file does the translation and untranslation of files.
 """
 
+import char_translator
+import os.path
+
 class Coder:
-    def __init__(self):
+    def __init__(self, ts_obj):
+        self.obj = ts_obj
 
-    def encode_file(self):
+    def encode_file(self, in_file, out_file):
+        """ empty """
+        self.file_conversion(in_file, out_file, self.obj.translate_char)
 
-    def decode_file(self):
+    def decode_file(self, in_file, out_file):
+        """ empty """
+        self.file_conversion(in_file, out_file, self.obj.untranslate_char)
+
+    def file_conversion(self, in_file, out_file, char_func):
+        try:
+            f_in = open(in_file, 'r')
+        except:
+            raise Exception("Not a valid file: {}".format(in_file))
+        f_out = open(out_file, 'w')
+
+        for line in f_in:
+            wr_line = ""
+            for char in line:
+                wr_line += char_func(char)
+            f_out.write(wr_line)
+        f_in.close()
+        f_out.close()
+
