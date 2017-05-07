e2ea8914019621e1704c0bc2e81abcac9dc59676
Gerald Shin <gerald.shin@purestorage.com>
2017-05-07 10:45:25 -0700

    modified crypto.py file_conversion to use 'with'

--- lab3_cryptography/crypto.py
+++ lab3_cryptography/crypto.py
@@ -6,27 +6,26 @@ This file does the translation and untranslation of files.
 import char_translator
 
 class Coder:
+    """ takes a CharTranslator object as its argument and stores it """
     def __init__(self, ts_obj):
         self.obj = ts_obj
 
+    """ takes an input and output file name and encodes the input file,
+    storing the output in the output file. """
     def encode_file(self, in_file, out_file):
         self.file_conversion(in_file, out_file, self.obj.translate_char)
 
+    """ takes an input and output file name and decodes the input file,
+    storing the output in the output file. """
     def decode_file(self, in_file, out_file):
         self.file_conversion(in_file, out_file, self.obj.untranslate_char)
 
+    """ file conversion of in_file to out_file depending char_func passed """
     def file_conversion(self, in_file, out_file, char_func):
-        try:
-            f_in = open(in_file, 'r')
-        except:
-            raise Exception("Not a valid file: {}".format(in_file))
-        f_out = open(out_file, 'w')
-
-        for line in f_in:
-            wr_line = ""
-            for char in line:
-                wr_line += char_func(char)
-            f_out.write(wr_line)
-        f_in.close()
-        f_out.close()
-
+        with open(in_file, 'r') as f_in:
+            with open(out_file, 'w') as f_out:
+                for line in f_in:
+                    wr_line = ""
+                    for char in line:
+                        wr_line += char_func(char)
+                    f_out.write(wr_line)
